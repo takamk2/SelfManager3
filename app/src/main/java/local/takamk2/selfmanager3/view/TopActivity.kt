27faf7f4751f026e3143a6d089fb1000be63965a
takamk2 <y.m.t.m.s.a.s@gmail.com>
2018-04-29 21:33:07 +0900

    Remove sample of LiveData

--- app/src/main/java/local/takamk2/selfmanager3/view/TopActivity.kt
+++ app/src/main/java/local/takamk2/selfmanager3/view/TopActivity.kt
@@ -1,12 +1,8 @@
 package local.takamk2.selfmanager3.view
 
-import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.jakewharton.rxbinding2.view.RxView
-import kotlinx.android.synthetic.main.activity_top.*
 import local.takamk2.selfmanager3.R
 import local.takamk2.selfmanager3.viewmodel.TopActivityVM
 
@@ -19,28 +15,5 @@ class TopActivity : AppCompatActivity() {
         setContentView(R.layout.activity_top)
 
         vm = ViewModelProviders.of(this).get(TopActivityVM::class.java)
-
-        // データをViewModelにあるdataに送る
-        RxView.clicks(redButton).subscribe { vm.postData(1) }
-        RxView.clicks(yellowButton).subscribe { vm.postData(2) }
-        RxView.clicks(blueButton).subscribe { vm.postData(3) }
-
-        // ViewModelにあるdataを監視。dataが更新されたらViewを更新する
-        vm.data.observe(this, Observer {
-            var color = Color.BLACK
-            when(it) {
-                "RED" -> {
-                    color = Color.RED
-                }
-                "YELLOW" -> {
-                    color = Color.YELLOW
-                }
-                "BLUE" -> {
-                    color = Color.BLUE
-                }
-            }
-            messageText.setTextColor(color) // Viewの見た目の更新はここに記述する
-            messageText.text = it
-        })
     }
 }
--- app/src/main/java/local/takamk2/selfmanager3/viewmodel/TopActivityVM.kt
+++ app/src/main/java/local/takamk2/selfmanager3/viewmodel/TopActivityVM.kt
@@ -2,29 +2,5 @@ package local.takamk2.selfmanager3.viewmodel
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.MutableLiveData
 
-class TopActivityVM(application: Application) : AndroidViewModel(application) {
-
-    val data = MutableLiveData<String>()
-
-    init {
-        data.value = "None" // デフォルト値
-    }
-
-    fun postData(id: Int) {
-        var str = "NONE"
-        when(id) { // サンプルなので雑...
-            1 -> {
-                str = "RED"
-            }
-            2 -> {
-                str = "YELLOW"
-            }
-            3 -> {
-                str = "BLUE"
-            }
-        }
-        data.postValue(str) // View用にデータを変換して購読しているViewに通知
-    }
-}
+class TopActivityVM(application: Application) : AndroidViewModel(application)
