94f5b1a4ae4c7b71e9c8b38a713169c57133b68d
takamk2 <y.m.t.m.h.h.s@gmail.com>
2018-04-29 12:10:36 +0900

    Remove Sample code

--- app/src/main/java/local/takamk2/selfmanager3/TopActivity.kt
+++ app/src/main/java/local/takamk2/selfmanager3/TopActivity.kt
@@ -2,18 +2,11 @@ package local.takamk2.selfmanager3
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import timber.log.Timber
 
 class TopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Timber.d("onResume: START")
-        Timber.d("onResume: END")
     }
 }
